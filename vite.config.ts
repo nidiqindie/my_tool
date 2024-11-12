0d039d9b67708363d821ecebbdca3adbca14da77
nidiqindie <a13213133779@hotmail.com>
2024-11-12 18:05:46 +0800

    2.6

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base:'./',
+  base:'/my_tool/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
